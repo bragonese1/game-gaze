3e6af7c838b44c5f0afbc0cffbc8b58fd5a69938
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-05-12 17:57:27 -0500

    add header component

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 const myAPI = "84df1d68ff654bf6908d821f0a3cc225"
 const openMenuToggle = $(".menu-toggle");
 const closeMenuToggle = $(".menu-toggle__btn-close");
+const gameCardContainer = $(".game-card-container");
 
 // game search url: url: `https://api.rawg.io/api/games?key=${myAPI}&search={game-name}}`
 // NO NEED TO REPLACE A BLANK SPACE
 // id:716864
 $.ajax({
     // games?key=${API_KEY}&search=${query}
-    url: `https://api.rawg.io/api/games/3498?key=${myAPI}`,
+    url: `https://api.rawg.io/api/games?key=${myAPI}`,
     method: "GET"
 }).then(function (res) {
     console.log(res);
 });
 
 const openNav = function(){
-    $("#menu").css("height", "100%");
+    $("#menu").removeClass("closed").addClass("open");
 }
 
 const closeNav = function (){
-    $("#menu").css("height", "0%");
+    $("#menu").removeClass("open").addClass("closed");
 }
 
 openMenuToggle.on("click", openNav);
