9bcf3c8c8342f35fcbd8f410534249fbff0d3f2d
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-05-12 15:57:36 -0500

    modify header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const myAPI = "84df1d68ff654bf6908d821f0a3cc225"
+const openMenuToggle = $(".menu-toggle");
+const closeMenuToggle = $(".menu-toggle__btn-close");
 
 // game search url: url: `https://api.rawg.io/api/games?key=${myAPI}&search={game-name}}`
 // NO NEED TO REPLACE A BLANK SPACE
@@ -10,3 +12,14 @@ $.ajax({
 }).then(function (res) {
     console.log(res);
 });
+
+const openNav = function(){
+    $("#menu").css("height", "100%");
+}
+
+const closeNav = function (){
+    $("#menu").css("height", "0%");
+}
+
+openMenuToggle.on("click", openNav);
+closeMenuToggle.on("click", closeNav);
