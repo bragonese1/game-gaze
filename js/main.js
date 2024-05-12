98098fab852116c8a647dbda6ce8af4b32b3b1f3
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-05-12 13:16:14 -0500

    design game card

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ const myAPI = "84df1d68ff654bf6908d821f0a3cc225"
 
 // game search url: url: `https://api.rawg.io/api/games?key=${myAPI}&search={game-name}}`
 // NO NEED TO REPLACE A BLANK SPACE
+// id:716864
 $.ajax({
     // games?key=${API_KEY}&search=${query}
-    url: `https://api.rawg.io/api/games?key=${myAPI}&search={GAME NAME}`,
+    url: `https://api.rawg.io/api/games/3498?key=${myAPI}`,
     method: "GET"
 }).then(function (res) {
     console.log(res);
